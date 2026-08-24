826242551c7008bd2b0df9e235e14868673eef92
Johan G <jgastrin@gmail.com>
2026-08-24 09:30:38 +0200

    301 konsekvenskompassen.se -> valsnack.se

--- workers/app.ts
+++ workers/app.ts
@@ -8,6 +8,12 @@ const requestHandler = createRequestHandler(
 
 export default {
   fetch(request, env, ctx) {
+    // Gamla domänen 301:ar till valsnack.se med path/query intakta.
+    const url = new URL(request.url);
+    if (url.hostname.endsWith("konsekvenskompassen.se")) {
+      url.hostname = "valsnack.se";
+      return Response.redirect(url.toString(), 301);
+    }
     const routerContext = new RouterContextProvider();
     routerContext.set(cloudflareContext, { env, ctx });
     return requestHandler(request, routerContext);
